80e7c82662ef26396ff337003b77720aec7dd691
alfonsofonso <alfonsofonso@gmail.com>
2018-09-12 19:35:58 +0200

    Added some code to start doing things, man!

--- bot.js
+++ bot.js
@@ -1,41 +1,56 @@
-// Our Twitter library
-var Twit = require('twit');
+// need to use this OTHER twitter lib to post photos, sigh
+var conf = require('./config.js');
+var Twitter = require('node-twitter');
+var twitterRestClient = new Twitter.RestClient(
+  conf.consumer_key,
+  conf.consumer_secret,
+  conf.access_token,
+  conf.access_token_secret
+);
 
-// We need to include our configuration file
-var T = new Twit(require('./config.js'));
-
-// This is the URL of a search for the latest tweets on the '#mediaarts' hashtag.
-var mediaArtsSearch = {q: "#mediaarts", count: 10, result_type: "recent"}; 
-
-// This function finds the latest tweet with the #mediaarts hashtag, and retweets it.
-function retweetLatest() {
-	T.get('search/tweets', mediaArtsSearch, function (error, data) {
-	  // log out any errors and responses
-	  console.log(error, data);
-	  // If our search request to the server had no errors...
-	  if (!error) {
-	  	// ...then we grab the ID of the tweet we want to retweet...
-		var retweetId = data.statuses[0].id_str;
-		// ...and then we tell Twitter we want to retweet it!
-		T.post('statuses/retweet/' + retweetId, { }, function (error, response) {
-			if (response) {
-				console.log('Success! Check your bot, it should have retweeted something.')
-			}
-			// If there was an error with our Twitter call, we print it out here.
-			if (error) {
-				console.log('There was an error with Twitter:', error);
-			}
-		})
-	  }
-	  // However, if our original search request had an error, we want to print it out here.
-	  else {
-	  	console.log('There was an error with your hashtag search:', error);
-	  }
-	});
+// Helper function
+function cinvirtString(strIn) {
+	var strOut;
+	// Do things
+	strOut = strIn;
+	return strOut;
 }
 
-// Try to retweet something as soon as we run the program...
-retweetLatest();
-// ...and then every hour after that. Time here is in milliseconds, so
-// 1000 ms = 1 second, 1 sec * 60 = 1 min, 1 min * 60 = 1 hour --> 1000 * 60 * 60
-setInterval(retweetLatest, 1000 * 60 * 60);
+function listenToMasses() {
+  var twitterStreamClient = new Twitter.StreamClient(
+    conf.consumer_key,
+    conf.consumer_secret,
+    conf.access_token,
+    conf.access_token_secret
+  );
+
+  if (twitterStreamClient.isRunning())
+  {
+    twitterStreamClient.stop();
+  }
+
+  twitterStreamClient.start(['@tiquismiquisbot']);
+
+  twitterStreamClient.on('tweet', function(tweet) {
+      console.log('A new request is on the way');
+      // console.log(tweet);
+      // postSarcasm(tweet.user.screen_name, tweet.id_str, tweet.text);
+  });
+
+  twitterStreamClient.on('close', function() {
+      console.log('Connection closed.');
+  });
+  twitterStreamClient.on('end', function() {
+      console.log('End of Line.');
+  });
+  twitterStreamClient.on('error', function(error) {
+      console.log('Error: ' + (error.code ? error.code + ' ' + error.message : error.message));
+  });
+}
+
+// Main function
+listenToMasses();
+
+// Challenge 1
+var strTest = '';
+console.log(cinvirtString(strTest));
